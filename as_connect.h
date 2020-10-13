dced5273b1ccb44ca781e057e7613962629cfc86
max <mpano91@gmail.com>
2020-10-13 19:02:47 +0300

    cpp-as module

---
+++ as_connect.h
@@ -0,0 +1,42 @@
+/* 
+ * File:   as_connect.h
+ * Author: Kirill Scherba <kirill@scherba.ru>
+ *
+ * Created on January 12, 2016, 12:04 PM
+ */
+
+#ifndef ASCONNECT_H
+#define	ASCONNECT_H
+
+#include <angelscript.h>
+
+#if !(defined(_WIN32) || defined(_WIN64))
+
+typedef unsigned int DWORD;
+
+DWORD timeGetTime();
+int getch();
+
+#endif
+
+typedef void (*registerGlobalFuntions)(asIScriptEngine *engine) ;
+
+/**
+ * AngelScript module data
+ */
+typedef struct asData {
+    
+    asIScriptEngine *engine;
+    asIScriptContext *ctx;
+    DWORD timeOut;
+    
+} asData;
+
+asData *asEngineInit(const char *script, registerGlobalFuntions rgf = NULL);
+void asEngineDestroy(asData *data);
+
+int regScript(asIScriptEngine *engine, const char* script);
+
+int asCheckFuncError(asData *data, asIScriptFunction *func, int r);
+int ExecuteMain(asData *data, const char* decl);
+#endif	/* ANGELSCRIPT_H */
